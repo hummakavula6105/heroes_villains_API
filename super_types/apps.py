3eb62dceaa7132f125b541024c779b5861644bc2
Danielle <hummakavula6105@gmail.com>
2022-06-26 16:02:41 -0400

    Deleted heroes_villains app and created SuperType model super_type app per instructions + registered super_types and rest framework in project settings installed apps section

--- super_types/apps.py
+++ super_types/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HeroesVillainsConfig(AppConfig):
+class SuperTypesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'heroes_villains'
+    name = 'super_types'

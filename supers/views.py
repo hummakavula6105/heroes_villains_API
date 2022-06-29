4978ab20196253adbb7000d7f40eec45ea9e6441
Danielle <hummakavula6105@gmail.com>
2022-06-28 20:51:56 -0400

    Registered Super model with Admin site + completed POST functionality for supers

--- supers/views.py
+++ supers/views.py
@@ -1,14 +1,40 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import SupersSerializer
 from .models import Super
+from rest_framework import status
 
-@api_view(['GET'])
+import supers
+from .serializers import SupersSerializer
+from .models import Super
+
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    supers = Super.objects.all()
 
-    serializer = SupersSerializer(supers, many=True)
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SupersSerializer(supers, many=True)
+        return Response (serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-
-    return Response (serializer.data)
+@api_view(['GET', 'PUT', 'DELETE'])
+def supers_list(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SupersSerializer(super);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SupersSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
